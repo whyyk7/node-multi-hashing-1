3dcc65d75f2060ae5ab8052461763e6a682806a6
k7 <c76@qq.com>
2016-07-27 14:12:03 +0800

    bug修复

--- multihashing.cc
+++ multihashing.cc
@@ -22,8 +22,9 @@ extern "C" {
     #include "nist5.h"
     #include "sha1.h"
     #include "x15.h"
-	  #include "fresh.h"
+	#include "fresh.h"
     #include "Lyra2RE.h"
+    #include "lbry.h"
 }
 
 #define THROW_ERROR_EXCEPTION(x) NanThrowError(x)
@@ -566,6 +567,27 @@ NAN_METHOD(lyra2re2) {
     );
 }
 
+NAN_METHOD(lbry) {
+    NanScope();
+
+    if (args.Length() < 1)
+        return THROW_ERROR_EXCEPTION("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+
+    lbryhash(input, output);
+
+    NanReturnValue(
+        NanNewBufferHandle(output, 32)
+    );
+}
+
 void init(Handle<Object> exports) {
     exports->Set(NanNew<String>("quark"), NanNew<FunctionTemplate>(quark)->GetFunction());
     exports->Set(NanNew<String>("x11"), NanNew<FunctionTemplate>(x11)->GetFunction());
@@ -589,6 +611,7 @@ void init(Handle<Object> exports) {
     exports->Set(NanNew<String>("fresh"), NanNew<FunctionTemplate>(fresh)->GetFunction());
     exports->Set(NanNew<String>("lyra2re"), NanNew<FunctionTemplate>(lyra2re)->GetFunction());
     exports->Set(NanNew<String>("lyra2re2"), NanNew<FunctionTemplate>(lyra2re2)->GetFunction());
+    exports->Set(NanNew<String>("lbry"), NanNew<FunctionTemplate>(lbry)->GetFunction());
 }
 
 NODE_MODULE(multihashing, init)
